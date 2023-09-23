24f1a66d76dc6fd55cba255db51f7b9110987733
Pramesh <codingwithpramesh@gmail.com>
2023-09-23 22:00:07 +0545

    Add project files.

---
+++ MovieAPIs/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieAPIs.Models.Domain;
+
+namespace MovieAPIs.Controllers
+{
+
+
+    [Route("[Controller]")]
+    [ApiController]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbcontext _context;
+        public UserController(ApplicationDbcontext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+           var data = _context.UserData.ToList();
+          return Ok(data);
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var data = _context.UserData.ToList();
+            return Ok(data);
+        }
+    }
+}
---
+++ MovieAPIs/Models/Domain/ApplicationDbcontext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MovieAPIs.Models.Domain
+{
+    public class ApplicationDbcontext : IdentityDbContext<ApplicationUser>
+    {
+
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options): base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<User> UserData { get; set; }
+    }
+}
